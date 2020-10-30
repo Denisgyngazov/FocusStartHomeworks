22e0acda13d211c44bbdae025e029a84ccfdb589
Денис Гынгазов <denisgyngazov>
2020-10-30 12:38:52 +0700

    Создан проект homework-3 и добавлен в workspace, удалены зависимости main.storyboard

---
+++ homework-3/homework-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework-3
+//
+//  Created by Денис Гынгазов on 30.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
